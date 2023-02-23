8f7b2667e744e4236bfd920d8ee9f4053ccac443
margaret manyeki <maggiemanyeki6@gmail.com>
2023-02-23 18:08:01 +0300

    done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,3 +3,8 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
